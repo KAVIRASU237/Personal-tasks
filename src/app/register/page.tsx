daad1e631d9f4b87b36f0af4983b5c746aa0c8f4
KAVIRASU237 <kavirasu237@gmail.com>
2026-01-16 22:17:46 +0530

    Fix register page build error

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import RegisterForm from "./RegisterForm";
 
+export const dynamic = "force-dynamic";
+
 export default async function RegisterPage() {
     const userCount = await prisma.user.count();
 
